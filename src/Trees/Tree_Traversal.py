c9361115f8e895abbb582d4510223479218e9c16
priyadharshini6508@gmail.com <priyadharshini6508@email.com>
2024-09-15 23:29:13 -0700

    Tree Traversal

--- src/Trees/Tree_Traversal.py
+++ src/Trees/Tree_Traversal.py
@@ -3,19 +3,32 @@
 class TreeNode:
 
     def __init__(self, val=0, left=None, right=None):
-        delf.val = val
+        self.val = val
         self.left = left
         self.right = right
 
-    def InorderTraversal(root):
+    def PreorderTraversal(self, root):
         if not root:
             return
         print(root.val)
-        InorderTraversal(root.left)
-        InorderTraversal(root.right)
+        self.PreorderTraversal(root.left)
+        self.PreorderTraversal(root.right)
 
+    def InorderTraversal(self, root):
+        if not root:
+            return
+        self.InorderTraversal(root.left)
+        print(root.val)
+        self.InorderTraversal(root.right)
 
-# Binary Tree : [3,9,20,null,null,15,7]
+    def PostorderTraversal(self, root):
+        if not root:
+            return
+        self.PostorderTraversal(root.left)
+        self.PostorderTraversal(root.right)
+        print(root.val)
+
+    # Binary Tree : [3,9,20,null,null,15,7]
 
 node1 = TreeNode(3)
 node2 = TreeNode(9)
@@ -28,6 +41,9 @@ node1.right = node3
 node3.left = node4
 node3.right = node5
 
-root = node1
-
-InorderTraversal(root)
+print('PreorderTraversal of the Tree :')
+node1.PreorderTraversal(node1)
+print('InorderTraversal of the Tree :')
+node1.InorderTraversal(node1)
+print('PostorderTraversal of the Tree :')
+node1.PostorderTraversal(node1)
