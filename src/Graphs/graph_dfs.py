210c1d9dff437aa6c18642350a644c4b1261e904
priyadharshini6508@gmail.com <priyadharshini6508@email.com>
2024-09-16 22:54:18 -0700

    Graph dfs

--- src/Graphs/graph_dfs.py
+++ src/Graphs/graph_dfs.py
@@ -1,1 +1,31 @@
 # DFS in undirected Graph
+
+def add_edge(adj, s, t):
+    adj[s].append(t)
+    adj[t].append(s)
+
+def dfs(node, visited, adj):
+    visited[node] = 1
+    print(node)
+
+    for link in adj[node] :
+        if visited[link] != 1 :
+            dfs(link, visited, adj)
+
+# Input for Graph
+
+vertices = 5
+edges = [[1, 2], [1, 0], [2, 0], [2, 3], [2, 4]]
+
+adj = [[] for _ in range(vertices)] # Adjacent list representing the links / connections from each node
+visited = [0]*vertices
+
+for e in edges:
+    add_edge(adj,e[0],e[1])
+
+print('DFS of the Graph :')
+
+dfs(1, visited, adj)
+
+
+
