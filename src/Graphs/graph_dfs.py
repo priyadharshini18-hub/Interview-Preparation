21e21119fe7154535743c58aede8f6dac00fe078
priyadharshini6508@gmail.com <priyadharshini6508@email.com>
2024-09-17 00:42:25 -0700

    Graph dfs - optimised with SET

--- src/Graphs/graph_dfs.py
+++ src/Graphs/graph_dfs.py
@@ -6,11 +6,12 @@ def add_edge(adj, s, t):
     adj[t].append(s)
 
 def dfs(node, visited, adj):
-    visited[node] = 1
+    #visited[node] = 1
+    visited.add(node)
     print(node)
 
     for link in adj[node] : # Once a node is visited, perform dfs for its adjacent nodes
-        if visited[link] != 1 : # Skip if the adjacent nodes are visited already
+        if not visited.__contains__(link) : # Skip if the adjacent nodes are visited already
             dfs(link, visited, adj)
 
 # Input for Graph
@@ -19,7 +20,8 @@ vertices = 5
 edges = [[1, 2], [1, 0], [2, 0], [2, 3], [2, 4]]
 
 adj = [[] for _ in range(vertices)] # Adjacent list representing the links / connections from each node
-visited = [0]*vertices
+# visited = [0]*vertices    # Use set instead of array Because set 'contains' is O(1)
+visited = set()
 
 for e in edges:
     add_edge(adj,e[0],e[1])
@@ -27,6 +29,4 @@ for e in edges:
 print('DFS of the Graph :')
 
 dfs(1, visited, adj)
-
-
 
