5a66a5a1ae958440936503a64ffc4df757da892c
priyadharshini6508@gmail.com <priyadharshini6508@email.com>
2024-09-22 23:32:00 -0700

    Trie Implementation + Algorithm with comments

--- src/trie/trie_implementation.py
+++ src/trie/trie_implementation.py
@@ -15,37 +15,37 @@ class Trie :
         curr = self.root
         for letter in word :
             if letter not in curr.children :
-                curr.children[letter] = TrieNode()
+                curr.children[letter] = TrieNode() # Letter not present in the curr node children
             curr = curr.children[letter]
-        curr.end = True
+        curr.end = True # Indicates the end of the word
 
     def search_word(self, word):
         curr = self.root
         for letter in word :
             if letter not in curr.children :
-                print(word + ' is not found in Tire')
+                print(word + ' is not found in Tire') # Letter not present in the curr node children -> Not found
                 return
             curr = curr.children[letter]
         if curr.end == True :
             print(word + ' is found in Tire')
         else :
-            print(word + ' is not found in Tire')
+            print(word + ' is not found in Tire') # All letters present in the curr node children but end_flg is False -> Not found
 
     def prefix_word_list(self, prefix):
         word = ''
         curr =  self.root
         for letter in prefix :
             if letter not in curr.children :
-                print('Prefix - ' + prefix + ' not found in Trie')
+                print('Prefix - ' + prefix + ' not found in Trie') # Letter not present in the curr node children -> Not found
                 return None
             curr = curr.children[letter]
         print('\nPrefix wordlist :')
-        self.prefix_helper(word, curr, prefix)
+        self.prefix_helper(word, curr, prefix) # To print all the words in the subtree
 
     def prefix_helper(self, word, curr, prefix):
         if curr.end == True:
             print(prefix+word)
-        for child in curr.children :
+        for child in curr.children : # To visit all the children subtrees
             self.prefix_helper(word + str(child), curr.children[child], prefix)
 
 
