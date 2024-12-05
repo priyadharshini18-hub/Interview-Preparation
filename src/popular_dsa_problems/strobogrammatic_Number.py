b1af9fed7182223058be3efa3cc2969bbca0acf1
priyadharshini6508@gmail.com <priyadharshini6508@email.com>
2024-12-05 01:32:37 -0800

    Easy 246 : LC sheet

---
+++ src/popular_dsa_problems/strobogrammatic_Number.py
@@ -0,0 +1,42 @@
+# Strobogrammatic Number
+
+# rotated_numerals = [0, 1, -1, -1, -1, -1, 9, -1, 8, 6]
+# num = '18088'
+# left, right = 0, len(num) - 1
+#
+# while left <= right:
+#     left_numeral, right_numeral = int(num[left]), int(num[right])
+#     if rotated_numerals[left_numeral] != right_numeral:
+#         print('no')
+#         exit()
+#     left += 1
+#     right -= 1
+# print('yes')
+
+rotated = {0, 1, 8, 6, 9}
+num = ['18088', '69', '111', '962', '8']
+
+for n in num :
+    left = 0
+    right = len(n) - 1
+    strobo = 0
+
+    while left <= right :
+        left_ch = int(n[left])
+        right_ch = int(n[right])
+
+        if left_ch not in rotated or right_ch not in rotated :
+            strobo = 1
+            break
+
+        if (left_ch == 6 and right_ch != 9) or (left_ch == 9 and right_ch != 6) :
+            strobo = 1
+            break
+
+        left += 1
+        right -= 1
+
+    if strobo == 0 :
+        print(n, 'is a Strobo Grammatic Number')
+    else :
+        print(n, 'is not a Strobo Grammatic Number')
